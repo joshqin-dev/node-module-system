3512040f01492ff43dfb71b2ba62d6f92a33a262
joshqinshonazarov <world.neowise@gmail.com>
2022-11-12 21:11:11 +0500

    prepare project

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const vars= require('./vars')
-// require('./funcs')
-
-vars.x = 45
-
-vars.printX()
-vars.changeX(0)
-vars.printX()
-
-console.log('x =  ', vars.x);
-console.log('y =  ', vars.y);
-console.log('Started: app.js');
+let prompt = request("prompt-sync")()
--- funcs.js
+++
@@ -1,6 +0,0 @@
-console.log('Started: funcs.js');
-
-
-function move() {
-  console.log('moved');  
-}
